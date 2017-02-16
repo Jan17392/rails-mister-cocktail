960d00448a3fa66cf338fe3a68769915f0d957d6
Jan Runo <jan.runo@whu.edu>
2017-02-16 15:20:20 -0200

    adding an index root

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,6 @@
 class Ingredient < ApplicationRecord
-  has_many :doses
+  has_many :doses, :dependent => :restrict_with_exception
   has_many :cocktails, through: :doses
 
-  validates :name, presence: :true
-  validates :name, uniqueness: true
+  validates :name, presence: :true, uniqueness: true
 end
